10733b84ee0613e86498d8b99e31e84f4a08f7c9
Jdshan <mjadshan@gmail.com>
2024-04-08 20:28:02 +0530

    add  AuthController

---
+++ DESK_SERVER/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Base_Library.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Server_Library.Repos.RepoInterfaces;
+
+namespace DESK_SERVER.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController(IUserAccountRepo accountRepo) : ControllerBase
+	{
+		private readonly IUserAccountRepo _accountRepo = accountRepo;
+		[HttpPost("register")]
+		public async Task<IActionResult> CreateAsync(RegisterDTO registerDTO)
+		{
+			if (registerDTO == null) return BadRequest("Model is empty");
+			var result = await _accountRepo.CreateAsynsc(registerDTO);
+			return Ok(result);
+		}
+
+		[HttpPost("Login")]
+		public async Task<IActionResult> SignInAsync(LoginDTO login)
+		{
+			if (login == null) return BadRequest("Model is empty");
+			var result = await _accountRepo.SignInAsync(login);
+			return Ok(result);
+		}
+	}
+}
