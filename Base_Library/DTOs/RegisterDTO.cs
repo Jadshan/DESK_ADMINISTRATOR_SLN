7548af980b68ff0aa03c9787c5b87c71ce8cfab9
Jdshan <mjadshan@gmail.com>
2024-04-08 20:26:26 +0530

    update

--- Base_Library/DTOs/RegisterDTO.cs
+++ Base_Library/DTOs/RegisterDTO.cs
@@ -4,12 +4,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Base_Library.DTOs
 {
-	public class RegisterDTO : AccountBase
+	public class RegisterDTO : AccountBaseDTO
 	{
 		[Required]
 		[MinLength(5)]
 		[MaxLength(100)]
 		public string? FullName { get; set; }
+
+		[Required]
+		[MinLength(5)]
+		[MaxLength(100)]
+		public string? UserName { get; set; }
 
 		[DataType(DataType.Password)]
 		[Compare(nameof(Password))]
