46cff742253946494cbbb9dde3adb8a63fc509bb
Jdshan <mjadshan@gmail.com>
2024-04-08 20:28:45 +0530

    update configurations

--- DESK_SERVER/Program.cs
+++ DESK_SERVER/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Server_Library.Data;
+using Server_Library.Helpers;
+using Server_Library.Repos.RepoInterfaces;
+using Server_Library.Repos.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +13,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+	options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
 //strating
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSqlConnection") ??
 	throw new InvalidOperationException("your connection not found!"));
 });
+
+builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
+
+//////
+builder.Services.AddScoped<IUserAccountRepo, AccountUserRepo>();
+///
 
 var app = builder.Build();
 
