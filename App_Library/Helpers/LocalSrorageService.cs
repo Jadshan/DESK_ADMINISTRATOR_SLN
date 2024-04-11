0bb4f27035973528647daecf033c264e588feb83
Jdshan <mjadshan@gmail.com>
2024-04-11 08:14:03 +0530

    LocalStorageService

---
+++ App_Library/Helpers/LocalSrorageService.cs
@@ -0,0 +1,15 @@
+﻿using Blazored.LocalStorage;
+
+namespace App_Library.Helpers
+{
+	public class LocalStorageService(ILocalStorageService localStorageService)
+	{
+		private readonly ILocalStorageService _localStorageService = localStorageService;
+		private const string storageKey = "auth-token";
+		public async Task<string> GetToken() => await _localStorageService.GetItemAsStringAsync(storageKey);
+		public async Task SetToken(string item) => await _localStorageService.SetItemAsStringAsync(storageKey, item);
+		public async Task RemoveToken() => await _localStorageService.RemoveItemAsync(storageKey);
+
+
+	}
+}
