ab426d4438a32e69a55f9b354fc8443f5b9d4981
Jdshan <mjadshan@gmail.com>
2024-04-11 08:14:38 +0530

    IUserAccountService interface

---
+++ App_Library/Services/Contracts/IUserAccountService.cs
@@ -0,0 +1,14 @@
+﻿using Base_Library.DTOs;
+using Base_Library.Responses;
+
+namespace App_Library.Services.Contracts
+{
+	public interface IUserAccountService
+	{
+		Task<GeneralResponse> CreateAsync(RegisterDTO user);
+		Task<LoginResponse> SignInAsync(LoginDTO user);
+		Task<LoginResponse> RefereshTokenAsync(RefereshTokenDTO tokenDTO);
+		Task<WeatherForecastDTO[]> GetWeatherForecasts();
+
+	}
+}
