965ccebe41af6cb54c76616d33aac5df2eb21819
Jdshan <mjadshan@gmail.com>
2024-04-11 08:15:49 +0530

    make configurations

--- DESK_MASTER_APP/Program.cs
+++ DESK_MASTER_APP/Program.cs
@@ -1,4 +1,9 @@
+using App_Library.Helpers;
+using App_Library.Services.Contracts;
+using App_Library.Services.Implementations;
+using Blazored.LocalStorage;
 using DESK_MASTER_APP;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +12,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<GetHttpClient>();
+builder.Services.AddScoped<LocalStorageService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<IUserAccountService, UserAccountService>();
 
 await builder.Build().RunAsync();
