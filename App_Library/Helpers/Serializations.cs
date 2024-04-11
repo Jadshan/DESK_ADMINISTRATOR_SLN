31fa63c92e83294547b32194d9d18560a6792045
Jdshan <mjadshan@gmail.com>
2024-04-11 08:14:19 +0530

    Serializations

---
+++ App_Library/Helpers/Serializations.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace App_Library.Helpers
+{
+	public class Serializations
+	{
+		public static string SerializeObj<T>(T modelObj) => JsonSerializer.Serialize(modelObj);
+
+		public static T DeserializeJsonString<T>(string jsonString) => JsonSerializer.Deserialize<T>(jsonString);
+
+		public static IList<T> DeserializeJsonStringList<T>(string jsonString) => JsonSerializer.Deserialize<IList<T>>(jsonString);
+	}
+}
+
