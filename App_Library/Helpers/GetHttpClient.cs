b241ed5e62d103b83c97b8a61c58a92a32650963
Jdshan <mjadshan@gmail.com>
2024-04-11 08:13:51 +0530

    GetHttpClient

---
+++ App_Library/Helpers/GetHttpClient.cs
@@ -0,0 +1,28 @@
+﻿using Base_Library.DTOs;
+
+namespace App_Library.Helpers
+{
+	public class GetHttpClient(IHttpClientFactory httpClientFactory, LocalStorageService localStorageService)
+	{
+		private const string headerKey = "auth";
+		public async Task<HttpClient> GetPrivateHttpClient()
+		{
+			var client = httpClientFactory.CreateClient("SystemApplicant");
+			var stringToken = await localStorageService.GetToken();
+			if (string.IsNullOrEmpty(stringToken)) return client;
+
+			var deserializeToken = Serializations.DeserializeJsonString<UserSessionDTO>(stringToken);
+			if (deserializeToken == null) return client;
+
+			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", deserializeToken.Token);
+			return client;
+		}
+
+		public HttpClient GetPublicHttpClient()
+		{
+			var client = httpClientFactory.CreateClient("SystemApplicant");
+			client.DefaultRequestHeaders.Remove(headerKey);
+			return client;
+		}
+	}
+}
